6f5dd9035719044078a0769abd53b02337cb2512
Amanda <150416071+mandltf@users.noreply.github.com>
2025-05-27 07:17:03 +0700

    Add files via upload

---
+++ mvc/mahasiswa.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2025 at 02:16 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `umur` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `biodata`
+--
+
+INSERT INTO `biodata` (`nim`, `nama`, `umur`) VALUES
+('123230130', 'mandi', 17),
+('123230131', 'manda', 18),
+('123230132', 'latifa', 19);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
